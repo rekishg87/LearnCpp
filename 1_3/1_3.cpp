390cbb000a4b5620d3200466e6039ede75824392
Rekish G <rekishg87@gmail.com>
2016-10-05 15:36:49 +0200

    Final 1.3

--- 1_3/1_3.cpp
+++ 1_3/1_3.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 #include <iostream>
-
+/*
 int main()
 {
 	// ask user for a number
@@ -16,5 +16,51 @@ int main()
 	std::cin.ignore(32757, '\n'); 
 	// get one more char from the user
 	std::cin.get();
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	// ask user for a number
+	// this using declaration tells the compiler that cout should resolve to std::cout
+	using std::cout;
+	// so no std:: prefix is needed here!
+	cout << "Enter a number: ";
+	int x = 0;
+	//read number from console and store it in x
+	using std::cin;
+	cin >> x;
+	cout << "You entered " << x << std::endl;
+
+	// reset any error flags
+	cin.clear();
+	// ignore any characters in the input buffer until we find an enter character
+	cin.ignore(32757, '\n');
+	// get one more char from the user
+	cin.get();
+	return 0;
+}
+*/
+
+int main()
+{
+	// ask user for a number
+	// this using directive tells the compiler that we're using everthing in the std namespace!
+	using namespace std;
+	// so no std:: prefix is needed here!
+	cout << "Enter a number: ";
+	int x = 0;
+	//read number from console and store it in x
+	cin >> x;
+	cout << "You entered " << x << endl;
+
+	// reset any error flags
+	cin.clear();
+	// ignore any characters in the input buffer until we find an enter character
+	cin.ignore(32757, '\n');
+	// get one more char from the user
+	cin.get();
 	return 0;
 }
