93923959499bc526b302153a3cb0e73bf3d1f206
Rekish G <rekishg87@gmail.com>
2016-10-05 14:57:32 +0200

    Updated 1.3

--- 1_3/1_3.cpp
+++ 1_3/1_3.cpp
@@ -1,11 +1,20 @@
-// 1_3.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
-
+#include <iostream>
 
 int main()
 {
-    return 0;
-}
+	// ask user for a number
+	std::cout << "Enter a number: "; 
+	int x = 0;
+	//read number from console and store it in x
+	std::cin >> x; 
+	std::cout << "You entered " << x << std::endl;
 
+	// reset any error flags
+	std::cin.clear(); 
+	// ignore any characters in the input buffer until we find an enter character
+	std::cin.ignore(32757, '\n'); 
+	// get one more char from the user
+	std::cin.get();
+	return 0;
+}
