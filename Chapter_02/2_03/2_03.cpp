d919eea906e2b2ec37d95412adc31dbd059810be
Rekish G <rekishg87@gmail.com>
2016-10-12 08:52:55 +0200

    Final 2.03

---
+++ Chapter_02/2_03/2_03.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	cout << "bool:\t\t" << sizeof(bool) << " bytes" << endl;
+	cout << "char:\t\t" << sizeof(char) << " bytes" << endl;
+	cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes" << endl;
+	cout << "char16_t:\t" << sizeof(char16_t) << " bytes" << endl;
+	cout << "char32_t:\t" << sizeof(char32_t) << " bytes" << endl;
+	cout << "short:\t\t" << sizeof(short) << " bytes" << endl;
+	cout << "int:\t\t" << sizeof(int) << " bytes" << endl;
+	cout << "long:\t\t" << sizeof(long) << " bytes" << endl;
+	cout << "long long:\t" << sizeof(long long) << " bytes" << endl;
+	cout << "float:\t\t" << sizeof(float) << " bytes" << endl;
+	cout << "double:\t\t" << sizeof(double) << " bytes" << endl;
+	cout << "long double:\t" << sizeof(long double) << " bytes" << endl;
+
+	cin.clear();
+	cin.ignore(32757, '\n');
+	cin.get();
+    return 0;
+}
+
--- Chapter_02/2_03/stdafx.cpp
+++ Chapter_02/2_03/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// 2_01.pch will be the pre-compiled header
+// 2_03.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
